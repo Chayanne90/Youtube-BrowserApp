33b880e3b22cae9cb60dd77d7a82cbbbbf3b5f5e
chayanne19 <chayannepaulino@gmail.com>
2021-02-02 19:34:07 -0500

    Search component

---
+++ src/component/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form">
+					<div className="field">
+						<label>Video Search</label>
+						<input type="text" placeholder="search" />
+
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+
+export default SearchBar;
+
+
