a8d052bdc0cb8d6cceca914a5e3cde70d65db6b6
chayanne19 <chayannepaulino@gmail.com>
2021-02-09 17:27:27 -0500

    Component set up

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+
+	state = { term: '' }
+
+	onInputChange = (e) => {
+
+		this.setState({ term: e.target.value });
+	}
+
+	onFormSubmit = (e) => {
+
+		e.preventDefault();
+	}
+
+
 	render() {
 		return (
 			<div className="search-bar ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Video Search</label>
-						<input type="text" placeholder="search" />
-
+						<input type="text"
+							placeholder="Search"
+							value={this.state.term}
+							onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>
